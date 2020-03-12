1e861937c1f16acd651a32a842acca0ccd216974
Zak <zakadmaster@gmail.com>
2020-03-12 11:04:08 +0100

    Addition of index and dotenv

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello there")
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/api", (req, res) => {
+    res.json("api is")
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})

63e395f67bebe5969cda2b8b8a8a43694a826d42
Zak <zakadmaster@gmail.com>
2020-03-12 11:46:29 +0100

    wip

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 
 const cors = require("cors");
@@ -7,10 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 app.get("/api", (req, res) => {
-    res.json("api is")
+    res.json("api is running")
 })
 
 app.listen(PORT, () => {
